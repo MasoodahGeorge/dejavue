19cd8ef6b15cd27ad269b2af3e0522234ce40209
MasoodahGeorge <oodasbusiness@gmail.com>
2024-06-24 14:32:22 +0200

    added

---
+++ src/views/CarsView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="cars">
+
+    </div>
+  </template>
+
+  
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    
+    
+</style>
---
+++ src/views/ConsolesView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="consoles">
+
+    </div>
+  </template>
+
+  
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    
+    
+</style>
